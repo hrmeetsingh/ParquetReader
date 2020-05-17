bcf551cf1dcc1f08a6afc7692be39b0fce7ca055
Harmeet Singh <er.harmeetsingh@hotmail.com>
2020-05-17 12:02:56 +0530

    Refactoring and adding method to get metadata keyvalues

--- src/main/java/parquet/utils/ParquetReaderUtils.java
+++ src/main/java/parquet/utils/ParquetReaderUtils.java
@@ -6,6 +6,7 @@ import org.apache.parquet.column.page.PageReadStore;
 import org.apache.parquet.example.data.simple.SimpleGroup;
 import org.apache.parquet.example.data.simple.convert.GroupRecordConverter;
 import org.apache.parquet.hadoop.ParquetFileReader;
+import org.apache.parquet.hadoop.metadata.FileMetaData;
 import org.apache.parquet.hadoop.util.HadoopInputFile;
 import org.apache.parquet.io.ColumnIOFactory;
 import org.apache.parquet.io.MessageColumnIO;
@@ -16,13 +17,22 @@ import org.apache.parquet.schema.Type;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class ParquetReaderUtils {
 
+    private static final ParquetFileReader readFile(String filePath) throws IOException {
+        return ParquetFileReader.open(HadoopInputFile.fromPath(new Path(filePath), new Configuration()));
+    }
+
     public static Parquet getParquetData(String filePath) throws IOException {
         List<SimpleGroup> simpleGroups = new ArrayList<>();
-        ParquetFileReader reader = ParquetFileReader.open(HadoopInputFile.fromPath(new Path(filePath), new Configuration()));
-        MessageType schema = reader.getFooter().getFileMetaData().getSchema();
+        ParquetFileReader reader = readFile(filePath);
+        FileMetaData fileMetaData = reader.getFooter().getFileMetaData();
+
+        MessageType schema = fileMetaData.getSchema();
+        Map<String, String> keyValueMetadata = fileMetaData.getKeyValueMetaData();
+
         List<Type> fields = schema.getFields();
         PageReadStore pages;
         while ((pages = reader.readNextRowGroup()) != null) {
@@ -36,6 +46,6 @@ public class ParquetReaderUtils {
             }
         }
         reader.close();
-        return new Parquet(simpleGroups, fields);
+        return new Parquet(simpleGroups, fields, keyValueMetadata);
     }
 }
