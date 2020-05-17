fb51fd904209b1a02d4fe36728d3d54a6d37f589
Harmeet Singh <er.harmeetsingh@hotmail.com>
2020-05-17 12:06:23 +0530

    Refactoring and using method getkeyvaluemetadata

--- src/main/java/parquet/reader/ParquetReader.java
+++ src/main/java/parquet/reader/ParquetReader.java
@@ -6,6 +6,7 @@ import org.apache.parquet.example.data.simple.SimpleGroup;
 import java.io.IOException;
 import org.apache.parquet.schema.Type;
 import java.util.List;
+import java.util.Map;
 
 
 public class ParquetReader{
@@ -16,17 +17,24 @@ public class ParquetReader{
         Parquet parquet = ParquetReaderUtils.getParquetData(currentDirectory+"/src/main/resources/part-00000.parquet");
         List<Type> schemalist = parquet.getSchema();
         List<SimpleGroup> datagroup = parquet.getData();
+        Map<String, String> keyValueFileMetaData = parquet.getKeyValueMetadata();
 
-        System.out.println("Data ---->");
+        System.out.println("\nData ---->");
         for (SimpleGroup data : datagroup){
             System.out.println(data.toString());
         }
 
-        System.out.println("Schema ---->");
+        System.out.println("\nSchema ---->");
         for (Type schema: schemalist){
             System.out.println("Name: "+schema.getName());
             System.out.println("Original Type: "+schema.getOriginalType());
             System.out.println("Primitive Type: "+schema.asPrimitiveType());
+        }
+
+        System.out.println("\nFile metadata key values ---->");
+        for (Map.Entry<String, String> entry : keyValueFileMetaData.entrySet()) {
+            System.out.println("Key - "+ entry.getKey());
+            System.out.println("Value - " + entry.getValue());
         }
     }
 }
--- src/main/java/parquet/utils/Parquet.java
+++ src/main/java/parquet/utils/Parquet.java
@@ -4,14 +4,17 @@ import org.apache.parquet.example.data.simple.SimpleGroup;
 import org.apache.parquet.schema.Type;
 
 import java.util.List;
+import java.util.Map;
 
 public class Parquet {
     private List<SimpleGroup> data;
     private List<Type> schema;
+    private Map<String, String> keyValueMetadata;
 
-    public Parquet(List<SimpleGroup> data, List<Type> schema) {
+    public Parquet(List<SimpleGroup> data, List<Type> schema, Map<String, String> keyValueMetadata) {
         this.data = data;
         this.schema = schema;
+        this.keyValueMetadata = keyValueMetadata;
     }
 
     public List<SimpleGroup> getData() {
@@ -20,5 +23,9 @@ public class Parquet {
 
     public List<Type> getSchema() {
         return schema;
+    }
+
+    public Map<String, String> getKeyValueMetadata() {
+        return keyValueMetadata;
     }
 }
